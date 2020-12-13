9dd4e77a5c9231b673bc831ca5dd5de630bbbcb2
VirajBhartiya <56783451+Viraj-Bhartiya@users.noreply.github.com>
2020-12-13 18:38:17 +0530

    first commit

--- js/misc.js
+++ js/misc.js
@@ -5,5 +5,6 @@ const single = document.getElementById('.single');
 if(screen.width < 600){
   // items.style.display='block';
   where.style.display = 'block';
+  document.querySelector('.fourth').style.display  = 'none';
   // single.style.display = 'inline-block';
 }
