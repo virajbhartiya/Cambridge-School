a8dec35304c82905a620e02dcae28dc36a719942
VirajBhartiya <56783451+Viraj-Bhartiya@users.noreply.github.com>
2020-12-13 09:18:06 +0530

    first commit

---
+++ js/misc.js
@@ -0,0 +1,9 @@
+const where = document.querySelector('.where');
+const items = document.querySelector('.items');
+const single = document.getElementById('.single');
+
+if(screen.width < 600){
+  // items.style.display='block';
+  where.style.display = 'block';
+  // single.style.display = 'inline-block';
+}
